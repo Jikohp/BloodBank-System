1264406d24f2cd2632155e0599edf9245f2557fb
Jikohp <50245509+Jikohp@users.noreply.github.com>
2020-07-23 12:49:11 +0530

    Add files via upload

---
+++ BloodBank.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Donor
+{
+	 String Name;
+	 int id;
+	 String Adrress;
+	 String Bloodgrp;
+	 String contact no;
+	 String adrress;
+	 String email_id;
+	 String donorgender,donorage;
+	void registration()
+	{
+
+
+	}
+	void WithdrawDetails()
+	{
+
+	}
+}
+class Seeker
+{
+	String Name;
+	 int id;
+	 String Adrress;
+	 String Bloodgrp;
+	 String contact no;
+	 String adrress;
+	 String email_id;
+	 String seekergender,seekerage;
+}
+class BloodBank
+{
+	public static void main(String args[])
+	{
+		String name,adress,phoneno,website,email,location,bloodstock;
+		void purchaseblood()
+		{
+			
+		}
+	}
+}
